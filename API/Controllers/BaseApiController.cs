76c1800d5216758b80c7f8ba4858c36681bbdc76
Rajib Dey <DEYR02@myunitec.ac.nz>
2021-05-15 15:22:39 +1200

    End of Section 3

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,7 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-   [ApiController, Route("[controller]")]
+    [ApiController]
+    [ Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
 
---
+++ client-app/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header, List, ListItem } from 'semantic-ui-react';
+
+
+function App() {
+
+  const [Activities, setActivities] = useState([]);
+  
+  useEffect(()=>{
+    axios.get("http://localhost:5000/api/activities").then(response =>{
+    console.log(response);    
+    setActivities(response.data);
+    })
+  }, [])
+  return (
+    <div >
+      <Header as="h2" icon="users" content='Reactivities'/>
+        <List>
+            {Activities.map((activity:any) =>(
+              <ListItem key ={activity.id}>{activity.title}</ListItem>
+            ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
