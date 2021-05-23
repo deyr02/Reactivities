36e1c41ea5ea8d773018376132c907fb469e123b
Rajib Dey <DEYR02@myunitec.ac.nz>
2021-05-23 15:33:51 +1200

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import "semantic-ui-css/semantic.min.css";
 import App from './app/layout/App';
 import './app/layout/style.css';
+import { store, StoreContext } from './app/stores/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
- 
+ <StoreContext.Provider value={store}>
     <App />
+
+ </StoreContext.Provider>
  ,
   document.getElementById('root')
 );
