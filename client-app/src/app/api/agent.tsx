009c0de1c08907afe8c5d6916afa1bf6198be6e5
Rajib Dey <DEYR02@myunitec.ac.nz>
2021-06-06 01:42:55 +1200

    End of section 13

--- client-app/src/app/api/agent.tsx
+++ client-app/src/app/api/agent.tsx
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import {Activity} from '../models/activity';
+import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
 
 const sleep = (delay:number) =>{
@@ -11,15 +12,11 @@ const sleep = (delay:number) =>{
 }
 
 axios.defaults.baseURL = "http://localhost:5000/api";
-// axios.interceptors.response.use(response =>{
-//     return sleep(1000).then(()=>{
-//         return response;
-//     }).catch((error)=>{
-//         console.log(error);
-//         return Promise.reject(error)
-        
-//     })
-// })
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if(token) config.headers.Authorization = `Bearer ${token}`
+    return config;
+})
 axios.interceptors.response.use(async response =>{
  
     await sleep(1000);
@@ -81,8 +78,18 @@ const Activities = {
     delete: (id:string)=> requests.del<void>(`/activities/${id}`),
 }
 
+const Account = {
+    current: ()=> requests.get<User>('/account'),
+    login:(user: UserFormValues)=> requests.post<User>('/account/login', user),
+    register: (user: UserFormValues)=> requests.post<User>('/account/register', user)
+}
+
+
+
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -18,7 +18,7 @@ export default observer( function ActivityDashboard(){
     if(activityRegistry.size <=1) loadingActivities();
     }, [activityRegistry.size, loadingActivities])
     
-    if(activityStore.loadingInitial) return<LoadingComponent content="loading app"></LoadingComponent>
+    if(activityStore.loadingInitial) return<LoadingComponent content="loading activity"></LoadingComponent>
 
     return(
         <Grid>
