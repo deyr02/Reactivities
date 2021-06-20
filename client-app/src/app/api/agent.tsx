1b54337e9512b4808024b6570b0433f2916415cb
Rajib Dey <DEYR02@myunitec.ac.nz>
2021-06-21 01:30:06 +1200

    Heroku deploy

--- client-app/src/app/api/agent.tsx
+++ client-app/src/app/api/agent.tsx
@@ -13,16 +13,16 @@ const sleep = (delay:number) =>{
     })
 }
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.interceptors.request.use(config => {
     const token = store.commonStore.token;
     if(token) config.headers.Authorization = `Bearer ${token}`
     return config;
 })
 axios.interceptors.response.use(async response =>{
- 
-    await sleep(1000);
-     const pagination = response.headers['pagination'];
+    if(process.env.NODE_ENV === 'development') await sleep(1000);
+    
+    const pagination = response.headers['pagination'];
     if (pagination) {
         response.data = new PaginatedResult(response.data, JSON.parse(pagination));
         return response as AxiosResponse<PaginatedResult<any>>
